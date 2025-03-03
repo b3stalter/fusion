94e985078eaafcc9163618fb06a69789f32b4770
Brian Stalter <stalterb@congersfd.org>
2025-03-02 20:24:57 -0500

    fusion coding challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'fusion';
+  item: string = '';
+  stages = ['Start', 'Step 1', 'Step 2', 'Finish'];
+  stageList: any = [
+    ['Tiffany'],
+    ['Matt', 'Justin'],
+    ['Hugo'],
+    ['Bryan']
+  ];
+
+  constructor() {
+    // this.stages.forEach((stage) => {
+    //   this.stageList.push([]);
+    // });
+    // console.log(this.stageList);
+  }
+
+
+  addItem(stage: any, item: any) {
+    console.log(stage, item);
+    this.stageList[stage].push(item);
+    this.item = '';
+
+    console.log(this.stageList);
+  }
+
+  promoteItem(stage: any, item: any) {
+    console.log('clicked', stage, item);
+    this.stageList[stage].splice(this.stageList[stage].indexOf(item), 1);
+
+    if (stage !== this.stages.length - 1) {
+      this.stageList[stage + 1].push(item);
+    }
+
+  }
+
+  demoteItem(stage: any, item: any) {
+    console.log('clicked', stage, item);
+    this.stageList[stage].splice(this.stageList[stage].indexOf(item), 1);
+
+    if (stage > 0) {
+      this.stageList[stage - 1].push(item);
+    }
+    return false;
+  }
+
+
 }
